69ccf7cfe2e3236534cd34771a8fa3071ea04553
Chelsea Troy <chelsea.dommert@gmail.com>
2019-10-18 13:59:53 -0500

    Implement Reverse Polish Notation printing using the visitor pattern

---
+++ java/com/chelseasinterpreter/RpnPrinter.java
@@ -0,0 +1,45 @@
+package com.chelseasinterpreter;
+
+class RpnPrinter implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+                new Expr.Binary(
+                        new Expr.Literal(1),
+                        new Token(TokenType.PLUS, "+", null, 1),
+                        new Expr.Literal(2)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Binary(
+                        new Expr.Literal(4),
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(3)));
+
+        System.out.println(new RpnPrinter().print(expression));
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return expr.left.accept(this) + " " + expr.right.accept(this) + " " + expr.operator.lexeme;
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return expr.operator.toString() + " " + expr.right.toString();
+    }
+
+
+}
